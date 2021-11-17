031bd6f06c587198155a373eb572df82e8e450f7
Ignacio Cinalli <nachocinalli@gmail.com>
2021-11-17 13:53:29 -0300

    Add columns and click event control

--- templates/cards.jsx
+++ templates/cards.jsx
@@ -1,11 +1,15 @@
+import Adapt from 'core/js/adapt';
 import React from 'react';
 import { html, classes, compile, templates } from 'core/js/reactHelpers';
 
 export default function Cards (props) {
   const {
+    _hasClickButton,
+    _columns,
     onKeyDown,
     onClick
   } = props;
+  const hasColumns = _columns > 1;
 
   return (
 
@@ -14,7 +18,8 @@ export default function Cards (props) {
       <templates.header {...props} />
 
       <div className={classes([
-        'component__widget', 'cards__widget'])}>
+        'component__widget', 'cards__widget',
+        hasColumns && 'has-columns'])}>
 
         {props._items.map(({ title, body, description, _graphic, _classes, _hasAction, _buttons, _index, _isVisited, _isActive, _isAnimated }) =>
           <div
@@ -25,64 +30,70 @@ export default function Cards (props) {
               _isActive && 'is-active',
               _isAnimated && 'is-animating',
               _graphic.src && 'has-image',
+              !_hasClickButton && _hasAction && 'has-action',
               _classes
             ])}
             key={_index}
             data-index={_index}
-
+            onClick={!_hasClickButton && _hasAction ? onClick : null}
+            style={(hasColumns && Adapt.device.screenSize === 'large' && { width: `${100 / _columns}%` }) || null}
           >
-            <div className="cards__item-container">
-              { _graphic.src &&
+            <div className="cards__item-inner">
+              <div className="cards__item-container">
+                { _graphic.src &&
               <templates.image {..._graphic}
                 classNamePrefixes={['cards__item']}
                 attributionClassNamePrefixes={['cards']}
               />
-              }
-              {title &&
+                }
+                {title &&
               <div className="cards__item-title">
                 {html(compile(title))}
               </div>
-              }
-              {body &&
+                }
+                {body &&
               <div className="cards__item-body">
                 {html(compile(body))}
               </div>
-              }
-            </div>
+                }
+              </div>
 
-            <div className="cards__item-toolbar">
+              <div className="cards__item-toolbar">
 
-              <div className="cards__item-description">
-                { description._graphic.src &&
+                <div className="cards__item-description">
+                  { description._graphic.src &&
                   <templates.image {...description._graphic}
                     classNamePrefixes={['cards__item-description']}
                     attributionClassNamePrefixes={['cards']}
                   />
-                }
-                {description.title &&
+                  }
+                  {description.title &&
                 <div className="cards__item-description-title">
                   {html(compile(description.title))}
                 </div>
-                }
-                {description.body &&
+                  }
+                  {description.body &&
                 <div className="cards__item-description-body">
                   {html(compile(description.body))}
                 </div>
-                }
-              </div>
+                  }
+                </div>
 
-              {_hasAction && <div className="cards__item-actions">
-                <button onClick={onClick} onKeyDown={onKeyDown} className="btn-text" aria-label={_isActive ? _buttons._close.ariaLabel : _buttons._open.ariaLabel}>
-                  {html(compile(_isActive ? _buttons._close.buttonText : _buttons._open.buttonText))}
-                </button>
-              </div>
-              }
-              <div className="cards__item-state">
-                <div className="icon"></div>
+                {_hasAction && <div className="cards__item-actions">
+                  <button
+                    onClick={_hasClickButton ? onClick : null}
+                    onKeyDown={onKeyDown}
+                    className="btn-text" aria-label={_isActive ? _buttons._close.ariaLabel : _buttons._open.ariaLabel}>
+                    {html(compile(_isActive ? _buttons._close.buttonText : _buttons._open.buttonText))}
+                  </button>
+                </div>
+                }
+                <div className="cards__item-state">
+                  <div className="icon"></div>
+                </div>
               </div>
             </div>
           </div>
-
         )}
 
       </div>
